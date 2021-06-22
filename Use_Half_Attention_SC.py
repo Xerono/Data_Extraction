Fixed softmax error

Use_Half_Attention_SC.py
@@ -1,14 +1,12 @@
 #Variables
 
-Soils_Q = True # If false, Coordinate Model will be used
-Layernums = [0] # Layers from which attention should be used
+#Soils_Q = True # If false, Coordinate Model will be used
+#Layernums = [0] # Layers from which attention should be used
 
 Debug_Q = False # Output debug information
 
-
-
-
-
+Soils_Q_List = [True, False]
+Layernums_List = [[0], [5], [0, 1, 2, 3, 4, 5]]
 
 def create_Attdict(attention):
     AttentionDict = {}
@@ -234,224 +232,247 @@ def check_words(important_words):
 def get_label(Str, Model, Tokenizer):
     StrEnc = Tokenizer(Str, return_tensors='pt').to(device)
     Output = Model(**StrEnc)
-    Softmaxed = Output.logits.softmax(1)
+    Softmaxed = Output.logits.softmax(-1)
     Label = Softmaxed[0][1].item()
     return Label
 
-if not Soils_Q:
-    import re
-    def coord_regex(Stringlist):
-        Found = False
-        # Find xx°xx'N
-        rege1 = "[0-9][0-9].{1,4}[0-9][0-9].{1,4}(?:N|S).{1,5}[0-9].{0,10}[0-9][0-9].{1,4}(?:W|E).{1,1}"
-        # Find xx°xx'xx''N
-        rege2 = "[0-9][0-9].{1,4}[0-9][0-9].{1,4}[0-9][0-9].{1,4}(?:N|S).{1,5}[0-9].{0,6}[0-9][0-9].{1,4}(?:W|E).{1,1}"
-        regelist = [rege1, rege2]
-        for potcoord in Stringlist:
-            for regel in regelist:
-                results = re.findall(regel, potcoord)
-                if len(results)>0:
-                    Found = True
-        return Found
-
-
-
-
-import os
-
-CurDir = os.getcwd()
-
-
-
-
-
-import sqlite3
-
-Database = CurDir + "/Results/Results.db"
-if not os.path.isfile(Database):
-    Con = sqlite3.connect(Database)
-    Cur = Con.cursor()
-    sql_command = """
-            CREATE TABLE Results (
-            Model String NOT NULL,
-            Bew_1_Tat_1 INTEGER NOT NULL,
-            Bew_1_Tat_0 INTEGER NOT NULL,
-            Bew_0_Tat_1 INTEGER NOT NULL,
-            Bew_0_Tat_0 INTEGER NOT NULL,
-            B1_Soil_Found INTEGER NOT NULL,
-            B1_Soil_Not_Found INTEGER NOT NULL,
-            B0_Soil_Found INTEGER NOT NULL,
-            B0_Soil_Not_Found INTEGER NOT NULL,
-            Percentage NOT NULL,
-            PRIMARY KEY(Model, Percentage)
-            );"""
-    Cur.execute(sql_command)
-    Con.commit()
-    Con.close()
-
-
-
-Database = CurDir + "/Files/Database.db"
-Con = sqlite3.connect(Database)
-Cur = Con.cursor()
 
 
 
 
 
 
-Paragraphs = {}
-Type = "Half_Attention"
-Layers = ""
-for layer in Layernums:
-    Layers = Layers + "_L" + str(layer) 
-
-if Soils_Q:
-    Model_path = CurDir + "/Models/SC_Model_Soils/"
-    Model_Name = Type + Layers + "_Soils"
-    PercentageO = 90
-    xs = "Select FPID, Soil, SoilD FROM Soils"
-    SoilPars = Cur.execute(xs).fetchall()
-    xs = "Select * FROM Pars"
-    Pars = Cur.execute(xs).fetchall()
-    Con.close()
-
-    for (ParID, Soil, SoilD) in SoilPars:
-        if ParID not in Paragraphs.keys():
-            Paragraphs[ParID] = [(Soil, SoilD)]
-        else:
-            Paragraphs[ParID].append((Soil, SoilD))
-else:
-    Model_path = CurDir + "/Models/SC_Model_Coordinates/"
-    Model_Name = Type + Layers + "_Coords"
-    PercentageO = 70
-
-    xs = "Select FPID, NumOfCoords from Coordinates"
-    CoordsPars = Cur.execute(xs).fetchall()
-    xs = "Select * FROM Pars"
-    Pars = Cur.execute(xs).fetchall()
-    Con.close()
-    
-    for (ParID, NumOfCoords) in CoordsPars:
-        Paragraphs[ParID] = NumOfCoords
-
-
-
-Maxlength = 917
-
-Dataset = []
-LongPars = []
-for (FPID, File, Par) in Pars:
-    if len(Par) < Maxlength:
-        if FPID in Paragraphs.keys():
-            Dataset.append(((FPID, Par), 1))
-        else:
-            Dataset.append(((FPID, Par), 0))
-            Paragraphs[FPID] = []
-    else:
-        LongPars.append(Par)
-
-   
-import torch
-import transformers
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-Model = transformers.DistilBertForSequenceClassification.from_pretrained(Model_path, output_attentions = True)
-Model.eval()
-Model.to(device)
-
-from transformers import DistilBertTokenizerFast
-PreTrainedModel = "distilbert-base-uncased"
-Tokenizer = DistilBertTokenizerFast.from_pretrained(PreTrainedModel)
-
-
-Percentage = PercentageO/100
-
-Resultsdict = {}
-
-o1 = "NullEins"
-o0 = "NullNull"
-B1F = "B1SoilFound"
-B1N = "B1SoilNotF"
-B0F = "B0SoilFound"
-B0N = "B0SoilNotF"
-
-
-Resultsdict[11] = 0 # Bewertet 1, Label 1
-Resultsdict[10] = 0 # Bewertet 1, Label 0
-Resultsdict[o1] = 0
-Resultsdict[o0] = 0
-Resultsdict[B1F] = 0
-Resultsdict[B1N] = 0
-Resultsdict[B0F] = 0
-Resultsdict[B0N] = 0
 
 
 
 
-Status = 0
-for ((FPID, Par), Label) in Dataset:
-    enctext = Tokenizer(Par, return_tensors='pt').to(device)
-    CalcLabel = get_label(Par, Model, Tokenizer)
-    OverPerc = False
-    if Label == 1:
-        if CalcLabel > Percentage:
-            Resultsdict[11]+=1
-            OverPerc = True
-        else:
-            Resultsdict[o1]+=1
-    else:
-        if CalcLabel > Percentage:
-            Resultsdict[10]+=1
-            OverPerc = True
-        else:
-            Resultsdict[o0]+=1
-    if Label == 1:
-        imp_words_checked = split_check(Par, Model, Tokenizer, Percentage)
-        if Soils_Q:
-            for (Soil, SoilD) in Paragraphs[FPID]:
+
+
+
+
+
+
+
+
+
+
+for Soils_Q in Soils_Q_List:
+    for Layernums in Layernums_List:
+        print(Soils_Q)
+        print(Layernums)
+
+
+        if not Soils_Q:
+            import re
+            def coord_regex(Stringlist):
                 Found = False
-                for word in imp_words_checked:  
-                    if (Soil in word) or (SoilD in word) or word in Soil or word in SoilD:
-                        Found = True
-                if Found == True:
-                    if OverPerc:
-                        Resultsdict[B1F] += 1
-                    else:
-                        Resultsdict[B0F] += 1
+                # Find xx°xx'N
+                rege1 = "[0-9][0-9].{1,4}[0-9][0-9].{1,4}(?:N|S).{1,5}[0-9].{0,10}[0-9][0-9].{1,4}(?:W|E).{1,1}"
+                # Find xx°xx'xx''N
+                rege2 = "[0-9][0-9].{1,4}[0-9][0-9].{1,4}[0-9][0-9].{1,4}(?:N|S).{1,5}[0-9].{0,6}[0-9][0-9].{1,4}(?:W|E).{1,1}"
+                regelist = [rege1, rege2]
+                for potcoord in Stringlist:
+                    for regel in regelist:
+                        results = re.findall(regel, potcoord)
+                        if len(results)>0:
+                            Found = True
+                return Found
+        import os
+
+        CurDir = os.getcwd()
+
+
+
+
+
+        import sqlite3
+
+        Database = CurDir + "/Results/Results.db"
+        if not os.path.isfile(Database):
+            Con = sqlite3.connect(Database)
+            Cur = Con.cursor()
+            sql_command = """
+                    CREATE TABLE Results (
+                    Model String NOT NULL,
+                    Bew_1_Tat_1 INTEGER NOT NULL,
+                    Bew_1_Tat_0 INTEGER NOT NULL,
+                    Bew_0_Tat_1 INTEGER NOT NULL,
+                    Bew_0_Tat_0 INTEGER NOT NULL,
+                    B1_Soil_Found INTEGER NOT NULL,
+                    B1_Soil_Not_Found INTEGER NOT NULL,
+                    B0_Soil_Found INTEGER NOT NULL,
+                    B0_Soil_Not_Found INTEGER NOT NULL,
+                    Percentage NOT NULL,
+                    PRIMARY KEY(Model, Percentage)
+                    );"""
+            Cur.execute(sql_command)
+            Con.commit()
+            Con.close()
+
+
+
+        Database = CurDir + "/Files/Database.db"
+        Con = sqlite3.connect(Database)
+        Cur = Con.cursor()
+
+
+
+
+
+
+        Paragraphs = {}
+        Type = "Half_Attention"
+        Layers = ""
+        for layer in Layernums:
+            Layers = Layers + "_L" + str(layer) 
+
+        if Soils_Q:
+            Model_path = CurDir + "/Models/SC_Model_Soils/"
+            Model_Name = Type + Layers + "_Soils"
+            PercentageO = 90
+            xs = "Select FPID, Soil, SoilD FROM Soils"
+            SoilPars = Cur.execute(xs).fetchall()
+            xs = "Select * FROM Pars"
+            Pars = Cur.execute(xs).fetchall()
+            Con.close()
+
+            for (ParID, Soil, SoilD) in SoilPars:
+                if ParID not in Paragraphs.keys():
+                    Paragraphs[ParID] = [(Soil, SoilD)]
                 else:
-                    if OverPerc:
-                        Resultsdict[B1N] += 1
-                    else:
-                        Resultsdict[B0N] += 1
+                    Paragraphs[ParID].append((Soil, SoilD))
         else:
-            for i in range(Paragraphs[FPID]):
-                Found = coord_regex(imp_words_checked)
-                if Found == True:
-                    if OverPerc:
-                        Resultsdict[B1F] += 1
-                    else:
-                        Resultsdict[B0F] += 1
+            Model_path = CurDir + "/Models/SC_Model_Coordinates/"
+            Model_Name = Type + Layers + "_Coords"
+            PercentageO = 70
+
+            xs = "Select FPID, NumOfCoords from Coordinates"
+            CoordsPars = Cur.execute(xs).fetchall()
+            xs = "Select * FROM Pars"
+            Pars = Cur.execute(xs).fetchall()
+            Con.close()
+            
+            for (ParID, NumOfCoords) in CoordsPars:
+                Paragraphs[ParID] = NumOfCoords
+
+
+
+        Maxlength = 917
+
+        Dataset = []
+        LongPars = []
+        for (FPID, File, Par) in Pars:
+            if len(Par) < Maxlength:
+                if FPID in Paragraphs.keys():
+                    Dataset.append(((FPID, Par), 1))
                 else:
-                    if OverPerc:
-                        Resultsdict[B1N] += 1
-                    else:
-                        Resultsdict[B0N] += 1
-    Status+=1
-    if Status % 100 == 0:
-        print(str(Status) + " von " + str(len(Dataset)))
+                    Dataset.append(((FPID, Par), 0))
+                    Paragraphs[FPID] = []
+            else:
+                LongPars.append(Par)
 
-results_list = []
+           
+        import torch
+        import transformers
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-results_list.append((Model_Name, Resultsdict[11], Resultsdict[10],
-                     Resultsdict[o1], Resultsdict[o0], Resultsdict[B1F],
-                     Resultsdict[B1N], Resultsdict[B0F], Resultsdict[B0N], PercentageO))
+        Model = transformers.DistilBertForSequenceClassification.from_pretrained(Model_path, output_attentions = True)
+        Model.eval()
+        Model.to(device)
 
-Database = CurDir + "/Results/Results.db"
-Con = sqlite3.connect(Database)
-Cur = Con.cursor()
-sql_command = "INSERT INTO Results VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
-Cur.executemany(sql_command, results_list)
-Con.commit()
-Con.close()
-print("Finished")
+        from transformers import DistilBertTokenizerFast
+        PreTrainedModel = "distilbert-base-uncased"
+        Tokenizer = DistilBertTokenizerFast.from_pretrained(PreTrainedModel)
+
+
+        Percentage = PercentageO/100
+
+        Resultsdict = {}
+
+        o1 = "NullEins"
+        o0 = "NullNull"
+        B1F = "B1SoilFound"
+        B1N = "B1SoilNotF"
+        B0F = "B0SoilFound"
+        B0N = "B0SoilNotF"
+
+
+        Resultsdict[11] = 0 # Bewertet 1, Label 1
+        Resultsdict[10] = 0 # Bewertet 1, Label 0
+        Resultsdict[o1] = 0
+        Resultsdict[o0] = 0
+        Resultsdict[B1F] = 0
+        Resultsdict[B1N] = 0
+        Resultsdict[B0F] = 0
+        Resultsdict[B0N] = 0
+
+
+
+
+        Status = 0
+        for ((FPID, Par), Label) in Dataset:
+            enctext = Tokenizer(Par, return_tensors='pt').to(device)
+            CalcLabel = get_label(Par, Model, Tokenizer)
+            OverPerc = False
+            if Label == 1:
+                if CalcLabel > Percentage:
+                    Resultsdict[11]+=1
+                    OverPerc = True
+                else:
+                    Resultsdict[o1]+=1
+            else:
+                if CalcLabel > Percentage:
+                    Resultsdict[10]+=1
+                    OverPerc = True
+                else:
+                    Resultsdict[o0]+=1
+            if Label == 1:
+                imp_words_checked = split_check(Par, Model, Tokenizer, Percentage)
+                if Soils_Q:
+                    for (Soil, SoilD) in Paragraphs[FPID]:
+                        Found = False
+                        for word in imp_words_checked:  
+                            if (Soil in word) or (SoilD in word) or word in Soil or word in SoilD:
+                                Found = True
+                        if Found == True:
+                            if OverPerc:
+                                Resultsdict[B1F] += 1
+                            else:
+                                Resultsdict[B0F] += 1
+                        else:
+                            if OverPerc:
+                                Resultsdict[B1N] += 1
+                            else:
+                                Resultsdict[B0N] += 1
+                else:
+                    for i in range(Paragraphs[FPID]):
+                        Found = coord_regex(imp_words_checked)
+                        if Found == True:
+                            if OverPerc:
+                                Resultsdict[B1F] += 1
+                            else:
+                                Resultsdict[B0F] += 1
+                        else:
+                            if OverPerc:
+                                Resultsdict[B1N] += 1
+                            else:
+                                Resultsdict[B0N] += 1
+            Status+=1
+            if Debug_Q and Status % 100 == 0:
+                print(str(Status) + " von " + str(len(Dataset)))
+
+        results_list = []
+
+        results_list.append((Model_Name, Resultsdict[11], Resultsdict[10],
+                             Resultsdict[o1], Resultsdict[o0], Resultsdict[B1F],
+                             Resultsdict[B1N], Resultsdict[B0F], Resultsdict[B0N], PercentageO))
+
+        Database = CurDir + "/Results/Results.db"
+        Con = sqlite3.connect(Database)
+        Cur = Con.cursor()
+        sql_command = "INSERT INTO Results VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+        Cur.executemany(sql_command, results_list)
+        Con.commit()
+        Con.close()
+        
+        print("Finished")